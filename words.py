3cf3069c46a74cdfe3530a35224cf3792964c507
Kaiser Soze <test@example.com>
2022-08-11 19:22:33 -0500

    Add word problem

---
+++ words.py
@@ -0,0 +1,15 @@
+input = """\
+In the vein of The Time Traveler’s Wife and Life After Life, 
+The Invisible Life of Addie LaRue is New York Times bestselling author V. E. Schwab’s
+genre-defying tour de force. A Life No One Will Remember. A Story You Will Never Forget.
+France, 1714: in a moment of desperation, a young woman makes a Faustian bargain to live 
+forever—and is cursed to be forgotten by everyone she meets. Thus begins the extraordinary 
+life of Addie LaRue, and a dazzling adventure that will play out across centuries and continents, 
+across history and art, as a young woman learns how far she will go to leave her mark on the world. 
+But everything changes when, after nearly 300 years, Addie stumbles across a young man in a hidden 
+bookstore and he remembers her name. """
+
+# Problem: Find the # of capitalized words in this paragraph
+
+def find_capitalized_letters():
+    return answer
